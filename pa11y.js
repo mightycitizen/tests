9952594fdb27b4a93a41fd59742ee35324d95071
Kristine Fernandez <kfernandez@mightycitizen.com>
2020-08-17 12:16:37 -0500

    make urls config more generic

--- pa11y.js
+++ pa11y.js
@@ -4,17 +4,17 @@ const rimraf      = require('rimraf');
 
 const { baseUrl, urls } = require('./urls');
 const runPa11y = async (path, index) => {
-    const url = baseUrl + path;    
-    
+    const url = baseUrl + path;
+
     const results = await pa11y(url, {
       runners: [
           'axe'
       ]
-    });    
+    });
     let fileName = path.replace(/\//g, '-');
     if (fileName === "") fileName = "home";
     let violations = results.issues.filter(issue => {
-      return issue.type === 'error' && issue.runnerExtras.impact === "serious"
+      return issue.type === 'error' && ['serious','critical'].includes(issue.runnerExtras.impact)
     });
     if (violations.length > 0){
       fs.writeFile('./pa11y/'+index+'-'+fileName+'.json', JSON.stringify(violations, null, 4), err => {
@@ -24,17 +24,17 @@ const runPa11y = async (path, index) => {
         }
         //file written successfully
       })
-    }    
-      
+    }
+
 }
 
 
 const runPa11yTests = () => {
   let index = 0;
-  for (const path of urls) {      
+  for (const path of urls) {
       runPa11y(path, index);
       index++;
   }
-  
+
 }
 rimraf('./pa11y/*', runPa11yTests);
--- urls.js
+++ urls.js
@@ -1,21 +1,11 @@
-const baseUrl = 'https://helpinsure.mightycitizen.dev/'
+const baseUrl = '[BASE_URL]'
 
 const urls = [
-    'app/home.html',
-    'app/generic.html',
-    'app/auto/index.html',
-    'frontend/auto/results',
-    'frontend/auto/compare?compare=2',
-    'frontend/auto/detail',
-    'app/residential/index.html',
-    'frontend/residential/results',
-    'frontend/residential/results/wind',
-    'frontend/residential/compare?compare=673,542,749',
-    'frontend/residential/compare/wind?compare=673,542,749',
-    'frontend/residential/detail',
-    'frontend/residential/detail/wind',
-    'frontend/portal',
-    'frontend/portal/policy'
+    '/',
+    '/general.html',
+    '/program.html',
+    '/article.html',
+    '/event.html'
   ];
 
 // const urls = [
@@ -24,5 +14,5 @@ const urls = [
 //     'auto/index.html',
 //     'residential/index.html'
 //   ];
-exports.baseUrl = baseUrl; 
+exports.baseUrl = baseUrl;
 exports.urls = urls;
