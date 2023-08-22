c3c91087125afff5f5b2fc8db0322a9f3a6bf376
Kristine Fernandez <kfernandez@KristinandezMac.lan>
2023-08-22 11:12:06 -0500

    adjust automated tests to point to storybook links

--- urls.js
+++ urls.js
@@ -1,14 +1,16 @@
 
-const baseUrl = 'http://localhost:3001/patterns/'
-const urls = [ 'pages-home/pages-home.rendered.html',
-  'pages-events-events-listing/pages-events-events-listing.rendered.html',
-  'pages-general-cookies/pages-general-cookies.rendered.html',
-  'pages-general-default/pages-general-default.rendered.html',
-  'pages-general-form/pages-general-form.rendered.html',
-  'pages-general-modal/pages-general-modal.rendered.html',
-  'pages-general-section-test/pages-general-section-test.rendered.html',
-  'pages-general-sidebar/pages-general-sidebar.rendered.html',
-  'pages-news-news-detail/pages-news-news-detail.rendered.html',
-  'pages-news-news-listing/pages-news-news-listing.rendered.html' ];
+const baseUrl = 'http://localhost:6011/iframe.html?args=&id='
+const urls = [ 
+  'pages-events-events-detail--default',
+  'pages-events-events-listing--events-listing'];
+
+  //'pages-home/pages-home.rendered.html',
+  // 'pages-general-default/pages-general-default.rendered.html',
+  // 'pages-general-form/pages-general-form.rendered.html',
+  // 'pages-general-modal/pages-general-modal.rendered.html',
+  // 'pages-general-section-test/pages-general-section-test.rendered.html',
+  // 'pages-general-sidebar/pages-general-sidebar.rendered.html',
+  // 'pages-news-news-detail/pages-news-news-detail.rendered.html',
+  // 'pages-news-news-listing/pages-news-news-listing.rendered.html' 
 exports.baseUrl = baseUrl;
 exports.urls = urls;
